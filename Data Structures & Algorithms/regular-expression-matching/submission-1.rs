de9eea8375920be0cfadd34024bbeb831e9d2799
Devansh Jain <81306949+devanshjain15@users.noreply.github.com>
2026-04-01 12:13:26 +0530

    Add: regular-expression-matching - submission-1

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-1.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn is_match(s: String, p: String) -> bool {
+        let m = s.len(); 
+        let n = p.len(); 
+
+        let s_bytes = s.as_bytes(); 
+        let p_bytes = p.as_bytes(); 
+
+        let mut dp = vec![vec![false; n + 1]; m + 1]; 
+        dp[m][n] = true; 
+        
+        for i in (0..=m).rev() { 
+            for j in (0..n).rev() { 
+                let c = i < m && (s_bytes[i] == p_bytes[j] || p_bytes[j] == b'.'); 
+                if j + 1 < n && p_bytes[j + 1] == b'*' { 
+                    dp[i][j] = dp[i][j + 2] || (c && dp[i + 1][j]); 
+                } else { 
+                    dp[i][j] = c && dp[i + 1][j + 1]; 
+                }
+            }
+        }
+
+        dp[0][0]
+    }
+}
