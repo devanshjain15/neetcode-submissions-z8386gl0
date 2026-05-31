2d40e21af2e7dcb79630af59091c2899b6204a31
Devansh Jain <81306949+devanshjain15@users.noreply.github.com>
2026-05-31 21:12:09 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+        let mut digits = digits; 
+        for i in (0..digits.len()).rev() { 
+            if digits[i] < 9 { 
+                digits[i] += 1; 
+                return digits; 
+            }
+
+            digits[i] = 0; 
+        }
+
+        digits.insert(0, 1); 
+        digits
+    }
+}
